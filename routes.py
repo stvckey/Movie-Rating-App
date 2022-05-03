85e9ee6e0cb1a2a5990f5bbac967370361a59e99
BIGSTVCK <stvckey@LAPTOP-PIPJ2082.localdomain>
2022-05-03 18:15:18 -0400

    an attempt to understand

---
+++ routes.py
@@ -0,0 +1,162 @@
+from app import app, db
+from models import User, Rating
+import flask
+import os
+import random
+import flask
+from flask_login import login_user, current_user, LoginManager, logout_user
+from flask_login.utils import login_required
+
+from dotenv import load_dotenv,find_dotenv
+load_dotenv(find_dotenv())
+
+from wikipedia import get_wiki_link
+from tmdb import get_movie_data
+
+login_manager = LoginManager()
+login_manager.login_view = "login"
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_name):
+    return User.query.get(user_name)
+
+MOVIE_IDS = [
+    82690, 635302, 79082, 634649, 635302
+]
+
+@app.route("/login")
+def login():
+    return flask.render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    username = flask.request.form.get("user")
+    user = User.query.filter_by(username=username).first()
+    if user:
+        login_user(user)
+        return flask.redirect(flask.url_for("landing"))
+
+    else:
+        return flask.jsonify({"status": 401, "reason": "Wrong username or password"})
+
+
+@app.route("/signup")
+def signup():
+    return flask.render_template("signup.html")
+
+
+@app.route("/signup", methods=["POST"])
+def signup_post():
+    username = flask.request.form.get("user")
+    user = User.query.filter_by(username=username).first()
+    if user:
+        pass
+    else:
+        user = User(username=username)
+        db.session.add(user)
+        db.session.commit()
+
+    return flask.redirect(flask.url_for("login"))
+
+
+@app.route("/")
+def landing():
+    if current_user.is_authenticated:
+        return flask.redirect("main")
+    return flask.redirect("login")
+
+
+MOVIE_IDS = [
+    82690, 635302, 79082, 634649, 635302
+]
+
+@app.route("/main")
+@login_required
+def index():
+    movie_id = random.choice(MOVIE_IDS)
+
+    # API calls
+    (title, tagline, genre, poster_image) = get_movie_data(movie_id)
+    wikipedia_url = get_wiki_link(title)
+
+    ratings = Rating.query.filter_by(movie_id=movie_id).all()
+
+    return flask.render_template(
+        "index.html",
+        title=title,
+        tagline=tagline,
+        genre=genre,
+        poster_image=poster_image,
+        wiki_url=wikipedia_url,
+        ratings=ratings,
+        movie_id=movie_id,
+    )
+
+@app.route("/rate", methods=["POST"])
+def rate():
+    data = flask.request.form
+    rating = data.get("rating")
+    comment = data.get("comment")
+    movie_id = data.get("movie_id")
+
+    new_rating = Rating(
+        username=current_user.username,
+        rating=rating,
+        comment=comment,
+        movie_id=movie_id,
+    )
+
+    db.session.add(new_rating)
+    db.session.commit()
+    return flask.redirect("main")
+
+
+@app.route("/get_reviews", methods=["GET"])
+@login_required
+def foo():
+    ratings = Rating.query.filter_by(username=current_user.username).all()
+    return flask.jsonify(
+        [
+            {
+                "rating": rating.rating,
+                "comment": rating.comment,
+                "movie_id": rating.movie_id,
+            }
+            for rating in ratings
+        ]
+    )
+
+
+@app.route("/save_reviews", methods=["POST"])
+def save_reviews():
+    data = flask.request.json
+    user_ratings = Rating.query.filter_by(username=current_user.username).all()
+    new_ratings = [
+        Rating(
+            username=current_user.username,
+            rating=r["rating"],
+            comment=r["comment"],
+            movie_id=r["movie_id"],
+        )
+        for r in data
+    ]
+    for rating in user_ratings:
+        db.session.delete(rating)
+    for rating in new_ratings:
+        db.session.add(rating)
+    db.session.commit()
+    return flask.jsonify("Ratings successfully saved")
+
+
+@app.route("/comments")
+def comments():
+    return flask.render_template("comments.html")
+
+
+if __name__ == "__main__":
+    app.run(
+
+        debug=True
+    )
---
+++ static/js/App.js
@@ -0,0 +1,93 @@
+//pass a review as a prop to display its information
+function Review(props) {
+    return (<div>
+        <h3>Movie ID: {props.movieID} </h3>
+        <h3>Your Rating: {props.rating}</h3>
+        <h3>Your Comment:</h3>
+        <h2>{props.comment}</h2>
+        <input type="number" onChange={props.onRate} value={props.rating} min="1" max="5"/>
+        <input value={props.comment} onChange={props.onEdit}/>
+        <button onClick={props.onDelete}>Delete</button>
+    </div>);
+}
+
+function App() {
+  //val is a refrence to our variable and setVal is a function to change all arguments using the name "val"
+    const[val, setVal] = React.useState([])
+
+
+
+
+    function handleDelete(i) {
+      setVal([...val.slice(0, i), ...val.slice(i+1)]);
+    }
+  
+    function handleRatingChange(i, e) {
+      const newReviews = val.slice();
+      newReviews[i].rating = e.target.value;
+      setVal(newReviews);
+    }
+  
+    function handleCommentChange(i, e) {
+      const newReviews = val.slice();
+      newReviews[i].comment = e.target.value;
+      setVal(newReviews);
+    }
+
+
+
+
+
+      //Posts review changes back to 'save_reviews' route
+      function onClickSave() {
+        fetch('/save_reviews', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          //makes sure the request body is of the same datatype as declared in headers
+          body: JSON.stringify(val),
+        })
+          //changes response object to json
+          .then((response) => response.json());
+      }
+      
+      //in order to generate all reviews that we have, we use .map()
+      const reviews = val.map(
+        //for all reviews in array of reviews (i), turn each review into a prop able to be passed to Review component
+        (review, i) => <Review 
+          //maps review attributes to new prop attributes
+          movieID={review.movie_id}
+          rating={review.rating}
+          comment={review.comment}
+          //functions applicable to the component
+          onDelete={() => handleDelete(i)}
+          onEdit={(e) => handleCommentChange(i, e)}
+          onRate={(e) => handleRatingChange(i, e)}
+        />);
+      
+      //get reviews as API response
+      React.useEffect(() => {
+        fetch('/get_reviews', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        })
+        //converts response to JSON object and sets val to data recieved
+          .then((response) => response.json())
+          .then((data) => {
+            setVal(data);
+          });
+      }, []);
+
+    return (
+        <div className="App">
+            {reviews}
+            <button onClick={onClickSave}>Save Changes</button>
+        </div>
+    )
+}
+
+const domContainer = document.querySelector('#react_app');
+ReactDOM.render(React.createElement(App), domContainer);
